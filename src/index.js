86b6189f616360be4ebc9193ca12a640664614c3
wooljam <woalskdl@gmail.com>
2021-10-04 16:39:22 +0900

    build 오류 해결 못하는 중... webpack_dev_server 에서 지정한 경로 뒤에 계속 /public을 붙여서 404에러 뜸...

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import Blog from "./main";
+
+console.log("start project by webpack");
+let myBlog = new Blog();
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+var path = require('path');
+
+module.exports = {
+    mode: "development",
+    entry: './src/index.js',
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            include: path.resolve(__dirname, 'src'),
+            use: {
+                loader: "babel-loader",
+                options: {
+                    presets : [
+                        ["env", {
+                            "targets" : {
+                                "browsers" : ["last 2 versions"]
+                            },
+                            "debug" : false
+                        }]
+                    ]
+                }
+            }
+        }]
+    },
+    devServer: {
+        historyApiFallback : true
+    }
+}
+
+/*
+    ** node project 생성
+    1. command >> 해당 위치에서 npm init
+    2. npm install webpack --save-dev // webpack 설치
+    3. webpack.config.js 설정
+    4. 설정에 맞게 경로 및 파일 설정 (entry, output 설정)
+    4. npm run start >> (빌드 및 webpack -cli 설치)
+
+    5. npm install babel-preset-env --save-dev >> babel 설치 (브라우저 버전에 맞게 compiling
+    6. webpack.config.js > babel 설정 >> rulse 부분
+    7. babel core, loader 간의 버전 호환 설정 주의
+ */
